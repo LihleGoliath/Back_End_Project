a332364030e98b12045c6f420f2b4e1e3e312aef
LihleGoliath <lihlegoliath2@gmail.com>
2022-08-11 12:08:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express"); // Used to set up a server
+const cors = require("cors"); // Used to prevent errors when working locally
+const bodyParser = require('body-parser');
+
+const app = express(); // Initialize express as an app variable
+app.set("port", process.env.PORT || 6969); // Set the port
+// app.use(express.json()); // Enable the server to handle JSON requests
+app.use(cors()); // Don't let local development give errors
+app.use(express.static("public"));
+app.use(express.json())
+ 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+//import userRoute
+const userRoute = require("./routes/userRoute");
+const productsRoute = require("./routes/ProductsRoute");
+const ordersRoute = require("./routes/OrdersRoute");
+const categoryRoute = require("./routes/categoryRoute");
+
+ 
+
+
+
+app.get("/", (req, res) => {
+    res.sendFile( __dirname + "/" + "index.html");
+});
+
+
+app.use("/users",userRoute);
+app.use("/products",productsRoute);
+app.use("/orders",ordersRoute);
+app.use("/category",categoryRoute);
+
+app.listen(app.get("port"), () => {
+    console.log(`http://localhost:${app.get("port")}`);
+    console.log("Press Ctrl+C to exit server");
+});
---
+++ routes/ProductsRoute.js
@@ -0,0 +1,144 @@
+const express = require("express");
+const router = express.Router();
+const con = require("../lib/db_connection");
+const middleware = require("../middleware/auth")
+const user = require("../routes/userRoute")
+
+router.get("/", (req, res) => {
+    try {
+        con.query("SELECT * FROM products", (err, result) => {
+            if (err) throw err;
+            res.send(result);
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error)
+    }
+});
+
+router.get("/:id",(req, res) => {
+  try {
+      con.query(`SELECT * FROM products WHERE product_id = '${req.params.id}}'`, (err, result) => {
+          if (err) throw err;
+          res.send(result);
+      });
+  } catch (error) {
+      console.log(error);
+      res.status(400).send(error)
+  }
+});
+
+router.post("/",(req,res) => {
+    // if(req.user.user_type === "admin" ){
+
+
+        const product = {
+          sku: req.body.sku,
+          name: req.body.name,
+          price: req.body.price,
+          weight: req.body.weight,
+          descriptions: req.body.descriptions,
+          thumbnail: req.body.thumbnail,
+          image: req.body.image,
+          category: req.body.category,
+          create_date:req.body.create_date,
+          stock:req.body.stock
+        } 
+        try {
+          let sql = "INSERT INTO products SET ?"
+          con.query(sql, product
+            , (err, result) => {
+              if (err) throw err.message;
+              res.send(result)})
+        } catch (error) {
+          res.send(error)
+          // console.log(error);
+        }
+    // }else{
+    //     res.send("Not ALLOWED")
+    // }
+});
+
+// router.patch("/",(req,res)=>{
+//   const {
+//      sku,
+//      name
+//   } = req.body
+//   try {
+   
+//     con.query(`SELECT * FROM products WHERE sku="${sku}" and name = "${name}"`,(err,result) => {
+//         if (err) throw err;
+//         res.send(result);
+//     });
+// } catch (error) {
+//     console.log(error);
+//     res.status(400).send(console.log(error))
+// }
+// })
+
+router.put("/:id",middleware,(req,res) => {
+    
+
+    if(req.user.user_type === "admin" ){
+  const {
+        sku,
+        name,
+        price,
+        weight,
+        descriptions,
+        thumbnail,
+        image,
+        category,
+        create_date,
+        stock
+  }=req.body
+
+try {
+   
+   
+    con.query(`UPDATE products SET  
+    sku ="${sku}",
+    name ="${name}",
+    price ="${price}",
+    weight ="${weight}",
+    descriptions ="${descriptions}",
+    thumbnail="${thumbnail}",
+    image ="${image}",
+    category ="${category}",
+    create_date ="${create_date}",
+    stock ="${stock}"
+    WHERE product_id ="${req.params.id}"
+    
+    
+    `, (err, result) => {
+        if (err) throw err.message;
+        res.send(result);
+    });
+} catch (error) {
+  console.log(error);
+  res.status(400).send(error)
+}
+}else{
+    res.send("Not ALLOWED")
+}
+});
+
+
+router.delete("/:id",(req,res)=> {
+    // if(req.user.user_type === "admin" ){
+  try {
+    con.query(`DELETE  FROM products WHERE product_id='${req.params.id}'`, (err, result) => {
+        if (err) throw err.message;
+        res.send(result);
+    });
+} catch (error) {
+    console.log(error);
+    res.status(400).send(error)
+}
+// }else{
+//     res.send("Not ALLOWED")
+// }
+    });
+
+
+module.exports = router;
