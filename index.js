8f90a7b04936009fa7f91693630359f5bbcbf947
LihleGoliath <lihlegoliath2@gmail.com>
2022-08-12 13:32:23 +0200

    changes

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const ordersRoute = require("./routes/OrdersRoute");
 const categoryRoute = require("./routes/categoryRoute");
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080/#/"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
@@ -34,7 +34,7 @@ app.get("/", (req, res) => {
    `)
 });
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080/#/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);
